7c6383ab58b7c23fdcaa2ef0f43c59ee1d3a2297
aziznal <aziznal.dev@gmail.com>
2025-03-16 01:55:49 +0300

    move fetch-user action to own file

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,20 +1,7 @@
 import { Page } from "@/lib/client/components/layout/Page";
-import { createDrizzleSupabaseClient } from "@/lib/db/drizzle";
-import { usersView } from "@/lib/db/drizzle/schema";
+import { fetchUser } from "./action";
 
 export const dynamic = "force-dynamic";
-
-async function fetchUser() {
-  "use server";
-
-  const db = await createDrizzleSupabaseClient();
-
-  const result = await db.rls((tx) => {
-    return tx.select().from(usersView);
-  });
-
-  return result[0];
-}
 
 export default async function HomePage() {
   const result = await fetchUser();
